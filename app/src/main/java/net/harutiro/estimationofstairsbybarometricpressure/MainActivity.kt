490701317752988eb19240423d8092066e15b8cf
harutiro <harutiro2027@outlook.jp>
2023-02-28 10:41:29 +0900

    update: そこそこ使えるやつになった。

--- app/src/main/java/net/harutiro/estimationofstairsbybarometricpressure/MainActivity.kt
+++ app/src/main/java/net/harutiro/estimationofstairsbybarometricpressure/MainActivity.kt
@@ -9,6 +9,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.math.BigDecimal
+import kotlin.math.floor
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
@@ -34,14 +36,18 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         if (event.sensor.type == Sensor.TYPE_PRESSURE) {
             sensorX = event.values[0]
 
-            findViewById<TextView>(R.id.textView).text = WalkingEstimation.noiseRemoved(sensorX.toDouble()).toString()
+            findViewById<TextView>(R.id.textView).text = WalkingEstimation.noiseRemoved(sensorX.toDouble(),WalkingEstimation.beforeData.sensorData).toString()
 
             val ans = WalkingEstimation.comeData(sensorX.toDouble())
 
             Log.d("main", ans.toString())
 
-            if(ans != "sampling"){
-                 findViewById<TextView>(R.id.textView2).text = ans
+            findViewById<TextView>(R.id.textView2).text = BigDecimal.valueOf(floor(ans * 1000000) / 1000000).toPlainString()
+
+            findViewById<TextView>(R.id.textView3).text = if(ans > -0.00002){
+                "登ってない"
+            }else{
+                "登ってる"
             }
 
         }
--- app/src/main/java/net/harutiro/estimationofstairsbybarometricpressure/WalkingEstimation.kt
+++ app/src/main/java/net/harutiro/estimationofstairsbybarometricpressure/WalkingEstimation.kt
@@ -2,57 +2,53 @@ package net.harutiro.estimationofstairsbybarometricpressure
 
 import android.icu.text.SimpleDateFormat
 import android.util.Log
+import androidx.core.app.RemoteInput.EditChoicesBeforeSending
 import java.util.*
 import kotlin.math.abs
 
 class WalkingEstimation {
 
-    var beforeData = SensorModel(0.0, Date(System.currentTimeMillis()))
+    var beforeData = SensorModel(1010.0, Date(System.currentTimeMillis()))
 
-    var getTime:Date = Date(System.currentTimeMillis())
+    var beforeAmount = 0.0
 
-
-    fun noiseRemoved(sensingData:Double):Double{
-        return beforeData.sensorData * 0.9 + sensingData * 0.1
+    fun noiseRemoved(sensingData:Double , beforeSensingData: Double):Double{
+        return beforeSensingData * 0.99 + sensingData * 0.01
     }
 
+    fun noiseRemoved2(sensingData:Double , beforeSensingData: Double):Double{
+        return beforeSensingData * 0.6 + sensingData * 0.4
+    }
 
-    //TODO:ノイズ除去を行う。
-    fun comeData(sensingData:Double):String{
+    fun amountOfChangeCalculation(sensorData:SensorModel):Double{
+        return (sensorData.sensorData - beforeData.sensorData) / (sensorData.time.time - beforeData.time.time)
+    }
 
-        Log.d("now",(Date(System.currentTimeMillis()).time - getTime.time).toString())
+    fun comeData(sensingData:Double):Double{
+
+//        Log.d("now",(Date(System.currentTimeMillis()).time - getTime.time).toString())
 
 
         //サンプリング周波数の調整
         //0.5秒に一回取得するように変更
-        if(Date(System.currentTimeMillis()).time - getTime.time < 100){
-            return "sampling"
-        }
-        getTime = Date(System.currentTimeMillis())
+//        if(Date(System.currentTimeMillis()).time - getTime.time < 100){
+//            return "sampling"
+//        }
+
+        //現在の情報の取得
+        val nowSensorModel = SensorModel(sensingData,Date(System.currentTimeMillis()))
 
         //ローパスフィルター
-        val noiseRemoval = beforeData.sensorData * 0.9 + sensingData * 0.1
+        nowSensorModel.sensorData = noiseRemoved(sensingData = nowSensorModel.sensorData , beforeSensingData = beforeData.sensorData )
+        Log.d("now",nowSensorModel.toString())
 
-        Log.d("now",noiseRemoval.toString())
+        //変化量の計算
+        var nowAmount = amountOfChangeCalculation(nowSensorModel)
+        nowAmount = noiseRemoved2(sensingData = nowAmount, beforeSensingData = beforeAmount)
 
+        beforeData = nowSensorModel
+        beforeAmount = nowAmount
 
-        //気圧センサーが前回より0.05の大きさなら
-        if(abs(noiseRemoval- beforeData.sensorData) > 0.03){
-            val diff = Date(System.currentTimeMillis()).time - beforeData.time.time
-
-
-            Log.d("WolkingEstimation", "nowTime: " + Date(System.currentTimeMillis()))
-            Log.d("WolkingEstimation", "diff: $diff")
-
-            //現在の状態を保存
-            beforeData = SensorModel(noiseRemoval, Date(System.currentTimeMillis()))
-
-            //もし3以上状態が続いていたら、止まっていると判断
-            return if(diff <= 3000){ "上昇もしくは下降" } else { "動いていない" }
-
-        }
-
-        //何も判定されなかったら動いていない。
-        return "動いていない"
+        return nowAmount
     }
 }
